280ac2b9f013aa6f51e6a1b0c08971340ac317e8
Tomas-Balciunas <tomas.balciunas@stud.kitm.lt>
2021-03-24 22:04:56 +0200

    fixes

--- view/pages/page.view.php
+++ view/pages/page.view.php
@@ -11,25 +11,15 @@
     <table>
         <tr>
             <th>Įmonės pavadinimas</th>
-            <th>Kodas</th>
-            <th>PVM Kodas</th>
             <th>Adresas</th>
-            <th>Tel.</th>
             <th>El.paštas</th>
-            <th>Veikla</th>
-            <th>Vadovas</th>
         </tr>
         <?php foreach ($tasks->fullList() as $task): ?>
         
         <tr>
             <td><a href="/atsiskaitymas/info/<?= $task['id'] ?>"><?= $task['pavadinimas']; ?></a></td>
-            <td><?= $task['kodas']; ?></td>
-            <td><?= $task['pvm_kodas']; ?></td>
             <td><?= $task['adresas']; ?></td>
-            <td><?= $task['telefonas']; ?></td>
             <td><?= $task['el_pastas']; ?></td>
-            <td><?= $task['veikla']; ?></td>
-            <td><?= $task['vadovas']; ?></td>
         </tr>
         
         <?php endforeach; ?>
